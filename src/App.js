db4130a2fbed7f8a74b5d0186f4777c91d22a051
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-05-24 16:39:58 -0600

    initial store map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Map from './pages/Map';
 import Data from './pages/Data';
+import TiendasMap from './pages/TiendasMap';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
           <Route path="/data" element={<Data />} />
+          <Route path="/tiendasMap" element={<TiendasMap />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
     </Router>
