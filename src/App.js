c0d4e0e259687933fb42d01be104ad2dd9aba17d
Jorge Adrian de la Garza Flores <jorgeadelagarzaflores@gmail.com>
2025-05-25 01:50:00 -0600

    removed unused files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Map from './pages/Map';
 import NotFound from './pages/NotFound';
 import Mapa from './pages/Tiendas/Index';
 import Location from './pages/Location/Index';
@@ -13,7 +12,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/map" element={<Map />} />
           <Route path="/mapa" element={<Mapa />} />
           <Route path="/location" element={<Location />} />
           <Route path="*" element={<NotFound />} />
